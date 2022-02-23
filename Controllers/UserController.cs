7915f7e153dc918a228467ec8d4ab2a12ba399de
alsalmiw <alsalmiw@gmail.com>
2022-02-23 12:50:11 -0800

    downloaded 4 entity framework core pakages

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace parent_learning_resources_webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
---
+++ Models/DTO/PasswordDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parent_learning_resources_webapi.Models.DTO
+{
+    public class PasswordDTO
+    {
+        
+    }
+}
